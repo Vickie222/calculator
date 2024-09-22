a9cd7ee98803a284032fadcd4adad06235189983
Vickie222 <104118961+Vickie222@users.noreply.github.com>
2024-09-22 04:14:02 +0100

    Update calController.js

--- controller/calController.js
+++ controller/calController.js
@@ -1,5 +1,9 @@
 exports.add = (req,res) => {
     const {num1,num2} = req.body;
+    if(typeof num1 !== 'number' || typeof num2 !== 'number'){
+        res.status(400).json({message: 'Supplied data must be a number'});
+                return;
+    }
     const sum = num1 + num2;
     res.json({result: sum});
 
@@ -7,23 +11,30 @@ exports.add = (req,res) => {
 
 exports.subtract = (req, res) => {
     const {num1,num2} = req.body;
-    const difference = num1 - num2;
-    if(typeof num1 !== 'number' && num2 !== 'number'){
+    if(typeof num1 !== 'number' || typeof num2 !== 'number'){
         res.status(400).json({message: 'Supplied data must be a number'});
                 return;
     }
+    const difference = num1 - num2;
     res.json({result: difference});
-
 };
 exports.multiply = (req, res) => {
     const {num1,num2} = req.body;
+    if(typeof num1 !== 'number' || typeof num2 !== 'number'){
+        res.status(400).json({message: 'Supplied data must be a number'});
+                return;
+    }
     const product = num1 * num2;
     res.json({result: product});
 
 };
 exports.divide = (req, res) => {
     const {num1,num2} = req.body;
-    if (num2 === 0) {
+    if(typeof num1 !== 'number' || typeof num2 !== 'number'){
+        res.status(400).json({message: 'Supplied data must be a number'});
+                return;
+    }
+    else if (num2 === 0) {
         return res.status(400).json({error: 'Division by Zero is not allowed'});
     }
     const quotient = num1 / num2;
